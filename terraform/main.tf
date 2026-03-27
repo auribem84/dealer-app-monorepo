4d1465591161d1b2a34dabcc212ae859deb8b4d2
Arturo Uribe <auribe21@Mac.lan>
2026-03-27 15:24:55 -0400

    refactoring main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,20 @@
-# 2. SERVICIO WEB (BACKEND)
 resource "render_web_service" "backend_api" {
-  name   = "sre-backend-service"
-  plan   = "free"
-  region = "oregon"
+  name     = "sre-backend-service"
+  plan     = "free"
+  region   = "oregon"
+  repo_url = var.repo_url
 
-  runtime_source = {
-    native_runtime = {
-      auto_deploy   = true
-      branch        = "main"
-      build_command = "pip install -r requirements.txt"
-      start_command = "uvicorn main:app --host 0.0.0.0 --port $PORT"
-      repo_url      = var.repo_url
-      runtime       = "python"
+  runtime_source {
+    native_runtime {
+      runtime       = "python"                           # required
+      branch        = "main"                             # required
+      auto_deploy   = true                               # optional but common
+      build_command = "pip install -r requirements.txt" # your build command
+      start_command = "uvicorn main:app --host 0.0.0.0 --port $PORT" # required
     }
+  }
+
+  env_vars = {
+    "NODE_ENV" = "production"
   }
 }
